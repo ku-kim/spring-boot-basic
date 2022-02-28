521822a7845092654b36d9a24bc5ed9c615fb66f
“kukim” <kukim.dev@gmail.com>
2022-02-28 17:35:18 +0900

    feat: 회원가입 기능
    - 단순 등록
    - html의 input을 통해 POST /members/new에 회원 이름 전송하여 회원가입

---
+++ src/main/java/kukim/tistory/com/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package kukim.tistory.com.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
