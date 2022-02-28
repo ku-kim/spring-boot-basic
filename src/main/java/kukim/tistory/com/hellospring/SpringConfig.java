a578ef9d7abf988469d9ffdcaa79fd7f0a7e1122
“kukim” <kukim.dev@gmail.com>
2022-02-28 21:45:23 +0900

    feat: JdbcTemplate 버전

--- src/main/java/kukim/tistory/com/hellospring/SpringConfig.java
+++ src/main/java/kukim/tistory/com/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package kukim.tistory.com.hellospring;
 
 import kukim.tistory.com.hellospring.repository.JdbcMemberRepository;
+import kukim.tistory.com.hellospring.repository.JdbcTemplateMemberRepository;
 import kukim.tistory.com.hellospring.repository.MemberRepository;
 import kukim.tistory.com.hellospring.repository.MemoryMemberRepository;
 import kukim.tistory.com.hellospring.service.MemberService;
@@ -28,7 +29,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 
