87889fcfa739b115e86bcb168f6c7c8b3535cc1b
“kukim” <kukim.dev@gmail.com>
2022-02-28 21:20:08 +0900

    feat: h2 DB 연결과 JDBC 리포지토리 변경

--- src/main/java/kukim/tistory/com/hellospring/SpringConfig.java
+++ src/main/java/kukim/tistory/com/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package kukim.tistory.com.hellospring;
 
+import kukim.tistory.com.hellospring.repository.JdbcMemberRepository;
 import kukim.tistory.com.hellospring.repository.MemberRepository;
 import kukim.tistory.com.hellospring.repository.MemoryMemberRepository;
 import kukim.tistory.com.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +27,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
+
 
 }
