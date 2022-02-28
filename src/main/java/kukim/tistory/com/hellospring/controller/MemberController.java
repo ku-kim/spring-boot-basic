19ba1a54c4bbfe733efbaac4b0d1054a2d171a8f
“kukim” <kukim.dev@gmail.com>
2022-02-28 16:27:34 +0900

    feat: 자바코드로 직접 스프링빈 등록
    - SprintConfig.java에 @Bean을 활용

---
+++ src/main/java/kukim/tistory/com/hellospring/controller/MemberController.java
@@ -0,0 +1,15 @@
+package kukim.tistory.com.hellospring.controller;
+
+import kukim.tistory.com.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
